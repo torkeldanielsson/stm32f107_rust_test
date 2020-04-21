46da82d415bc79610cb38806b984ac8a0483efba
Torkel Danielsson <torkel@voysys.se>
2020-04-21 23:51:17 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -11,37 +11,19 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 
 #[entry]
 fn main() -> ! {
-    let mut peripherals = stm32f107::Peripherals::take().unwrap();
-
-    peripherals.GPIOD.moder.modify(|_, w| {
-        w.moder8().output();
-        w.moder9().output();
-        w.moder10().output();
-        w.moder11().output();
-        w.moder12().output();
-        w.moder13().output();
-        w.moder14().output();
-        w.moder15().output()
-    });
+    // let mut peripherals = stm32f107::Peripherals::take().unwrap();
 
     loop {
-
         unsafe {
             // A magic address!
-            const GPIOE_BSRR: u32 = 0x48001018;
-    
-            // Turn on the "North" LED (red)
-            *(GPIOE_BSRR as *mut u32) = 1 << 9;
-    
-            // Turn on the "East" LED (green)
-            *(GPIOE_BSRR as *mut u32) = 1 << 11;
-    
-            // Turn off the "North" LED
-            *(GPIOE_BSRR as *mut u32) = 1 << (9 + 16);
-    
-            // Turn off the "East" LED
-            *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16);
-        }
+            const GPIOD_BSRR: u32 = 0x40011410;
 
+            // Turn on the led... maybe...
+            *(GPIOD_BSRR as *mut u32) = 1 << 0;
+            // Turn on the led... maybe...
+            *(GPIOD_BSRR as *mut u32) = 1 << 1;
+            // Turn on the led... maybe...
+            *(GPIOD_BSRR as *mut u32) = 1 << 2;
+        }
     }
 }
