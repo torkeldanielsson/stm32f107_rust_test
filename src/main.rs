c24762180fa4715d4c7b17f46b1283e6b0a9e621
Torkel Danielsson <torkel@voysys.se>
2020-04-21 21:18:26 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,35 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 fn main() -> ! {
     let mut peripherals = stm32f107::Peripherals::take().unwrap();
 
-    peripherals.GPIOD.odr.write(|w| unsafe { w.bits(1) });
+    peripherals.GPIOD.moder.modify(|_, w| {
+        w.moder8().output();
+        w.moder9().output();
+        w.moder10().output();
+        w.moder11().output();
+        w.moder12().output();
+        w.moder13().output();
+        w.moder14().output();
+        w.moder15().output()
+    });
 
-    loop {}
+    loop {
+
+        unsafe {
+            // A magic address!
+            const GPIOE_BSRR: u32 = 0x48001018;
+    
+            // Turn on the "North" LED (red)
+            *(GPIOE_BSRR as *mut u32) = 1 << 9;
+    
+            // Turn on the "East" LED (green)
+            *(GPIOE_BSRR as *mut u32) = 1 << 11;
+    
+            // Turn off the "North" LED
+            *(GPIOE_BSRR as *mut u32) = 1 << (9 + 16);
+    
+            // Turn off the "East" LED
+            *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16);
+        }
+
+    }
 }
