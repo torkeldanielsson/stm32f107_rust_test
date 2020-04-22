fb603a244a6b84e2514783464b513af2381b7081
Torkel Danielsson <torkel@voysys.se>
2020-04-22 13:25:54 +0900

    can blink, yeah

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,17 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 fn main() -> ! {
     let peripherals = stm32f107::Peripherals::take().unwrap();
     let gpiod = &peripherals.GPIOD;
+    let rcc = &peripherals.RCC;
+
+    rcc.apb2enr.write(|w| w.iopden().set_bit());
+    gpiod.crl.write(|w| 
+        w.mode0().output().cnf0().push_pull()
+    );
 
     loop {
         gpiod.bsrr.write(|w| w.bs0().set_bit());
         cortex_m::asm::delay(2000000);
-        gpiod.brr.write(|w| w.br13().set_bit());
+        gpiod.brr.write(|w| w.br0().set_bit());
         cortex_m::asm::delay(2000000);
     }
 }
