bb3a083c71af221c9e20d2e2a1b5a51a7faa9b30
Torkel Danielsson <torkel@voysys.se>
2020-04-22 11:38:30 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,13 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 
 #[entry]
 fn main() -> ! {
-    // let mut peripherals = stm32f107::Peripherals::take().unwrap();
+    let peripherals = stm32f107::Peripherals::take().unwrap();
+    let gpiod = &peripherals.GPIOD;
 
     loop {
-        unsafe {
-            // A magic address!
-            const GPIOD_BSRR: u32 = 0x40011410;
-
-            // Turn on the led... maybe...
-            *(GPIOD_BSRR as *mut u32) = 1 << 0;
-            // Turn on the led... maybe...
-            *(GPIOD_BSRR as *mut u32) = 1 << 1;
-            // Turn on the led... maybe...
-            *(GPIOD_BSRR as *mut u32) = 1 << 2;
-        }
+        gpiod.bsrr.write(|w| w.bs0().set_bit());
+        cortex_m::asm::delay(2000000);
+        gpiod.brr.write(|w| w.br13().set_bit());
+        cortex_m::asm::delay(2000000);
     }
 }
